645116e2ed08c5070e0b90e42fc40cc404e3d758
CarmenDAM <cceballose01@educantabria.es>
2022-01-12 13:24:33 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,26 +5,13 @@ import { Home } from './components/Home';
 import { PaginaListas } from './components/PaginaListas';
 import { Perfil } from './components/Perfil';
 import Header from './components/Header';
+import{MenuItems} from './data/MenuItems'
 
 export function App() {
   return (
     <Router>
-{
-/**
-   * function howMany(selectObject) {
-  let numberSelected = 0;
-  for (let i = 0; i < selectObject.options.length; i++) {
-    if (selectObject.options[i].selected) {
-      numberSelected++;
-    }
-  }
-  return numberSelected;
-}
-   */
-}
- 
-
       <Header />
+      
       <Route path="/" exact component={Home} />
       <Route path="/listas" component={PaginaListas} />
       <Route path="/perfil" component={Perfil} />
