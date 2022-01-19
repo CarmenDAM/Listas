216d12b1d90adcb03a32324b08d91d91ec90758f
CarmenDAM <cceballose01@educantabria.es>
2022-01-19 12:46:46 +0100

    Tabla Moviles

---
+++ src/data/DatosMoviles.js
@@ -0,0 +1,37 @@
+export const TitulosTablaMoviles = {
+  id: 'Modelo',
+  field1: 'Marca',
+  field2: 'S.O',
+  field3: 'Color',
+};
+
+// TODO: rellenar atributos que faltan para todos los coches
+export const DatosTablaMoviles = [
+  {
+    modelo: 'iPhone 13',
+    marca: 'Apple',
+    so: 'IOS 13',
+    color: 'Blanco',
+  },
+  {
+    modelo: 'Nokia 3310',
+    marca: 'Nokia',
+    so: 'Java',
+    color: 'Negro',
+  },
+  {
+    modelo: 'Samsung Galaxy Z Flip',
+    marca: 'Samsung',
+    so: 'Android 11',
+    color: 'Negro',
+    imagen:
+      'https://cdn.pocket-lint.com/r/s/1200x/assets/images/157975-phones-review-hands-on-samsung-galaxy-flip-3-review-image1-qgcb9vrlxi.jpg',
+    descripción: 'El futuro ha llegado',
+  },
+  {
+    modelo: 'Redmi Note 10',
+    marca: 'Xiaomi',
+    so: 'Miwi 12',
+    color: 'Negro',
+  },
+];
