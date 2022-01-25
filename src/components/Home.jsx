99eb66f71b1d20fd6cf09dbb788e4ac2ae04d4b7
CarmenDAM <cceballose01@educantabria.es>
2022-01-25 12:22:25 +0100

    Login

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,10 +6,22 @@ class Home extends React.Component {
     super(props);
     this.state = { user: '', password: '' };
     this.login = this.login.bind(this);
+    this.inputUser = React.createRef();
+    this.inputPassword = React.createRef();
   }
 
   login() {
-    this.setState({ user: 'Pepito', password: '1234' });
+    this.setState({
+      user: this.inputUser.current.value,
+      password: this.inputPassword.current.value,
+    });
+  }
+
+  componentDidMount() {
+    this.setState({
+      user: localStorage.getItem('user'),
+      password: localStorage.getItem('password'),
+    });
   }
 
   render() {
@@ -20,35 +32,48 @@ class Home extends React.Component {
     ) {
       return (
         <div className="main-site">
-          <h1>Bienvenido {this.state.user}</h1>
+          <h1>Bienvenido {this.state.user}!</h1>
         </div>
       );
     } else {
       return (
-        <Container>
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Usuario" />
-              <Form.Text className="text-muted">
-                Introduce tu email porfavor
-              </Form.Text>
-            </Form.Group>
+        <div className="main-site">
+          <h1>Bienvenido!</h1>
+          <Container>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Nombre de usuario o email: </Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Usuario"
+                  ref={this.inputUser}
+                />
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Contraseña</Form.Label>
-              <Form.Control type="password" placeholder="Contraseña" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Recordar contraseña" />
-            </Form.Group>
-            <Button variant="primary" type="button" onClick={this.login}>
-              Login
-            </Button>
-          </Form>
-        </Container>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Contraseña: </Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Contraseña"
+                  ref={this.inputPassword}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Recordarme" />
+              </Form.Group>
+              <Button variant="primary" type="button" onClick={this.login}>
+                Login
+              </Button>
+            </Form>
+          </Container>
+        </div>
       );
     }
+  }
+
+  componentWillUnmount() {
+    localStorage.setItem('user', this.state.user);
+    localStorage.setItem('password', this.state.password);
   }
 }
 
